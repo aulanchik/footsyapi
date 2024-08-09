86a5518c5bbbaae606f5193657caf85740fbc4ca
Artyom Ulanchik <artyom.u@outlook.com>
2024-08-09 07:14:43 +0100

    init project

---
+++ src/main/java/io/aulanchik/footsyapi/FootsyapiApplication.java
@@ -0,0 +1,13 @@
+package io.aulanchik.footsyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootsyapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootsyapiApplication.class, args);
+	}
+
+}
