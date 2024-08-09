9ce404115fc04e5df525263c2286cb4e81f773a8
Artyom Ulanchik <artyom.u@outlook.com>
2024-08-09 07:26:48 +0100

    Added BaseEntity

---
+++ src/main/java/io/aulanchik/footsyapi/entities/BaseEntity.java
@@ -0,0 +1,24 @@
+package io.aulanchik.footsyapi.entities;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@ToString
+@MappedSuperclass
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public abstract class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
